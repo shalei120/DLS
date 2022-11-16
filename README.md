35430be585428f1d82f088e1c5c010713a586ea7
shalei120 <shalei120@sina.com>
2022-11-16 13:27:36 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# DLS
-Discrete Latent Space
+# Discrete Latent Space (In progress......)
+
+
+## Installing Dependencies
+
+To install dependencies, create a conda or virtual environment with Python 3 and then run `pip install -r requirements.txt`. 
+
+## Running  
+
+Simply run `python3 main.py`. Make sure to include the `-save` flag if you want to save your model. You can also add parameters in the command line. The default values are specified below:
+
+```python
+parser.add_argument("--batch_size", type=int, default=32)
+parser.add_argument("--n_updates", type=int, default=5000)
+parser.add_argument("--n_hiddens", type=int, default=128)
+parser.add_argument("--n_residual_hiddens", type=int, default=32)
+parser.add_argument("--n_residual_layers", type=int, default=2)
+parser.add_argument("--embedding_dim", type=int, default=64)
+parser.add_argument("--n_embeddings", type=int, default=512)
+parser.add_argument("--beta", type=float, default=.25)
+parser.add_argument("--learning_rate", type=float, default=3e-4)
+parser.add_argument("--log_interval", type=int, default=50)
+```
+
+## Models
+
+The DLS has the following fundamental model components:
+
+1. An `Encoder` class which defines the map `x -> z_e`
+2. A `VectorQuantizer` class which transform the encoder output into a discrete one-hot vector that is the index of the closest embedding vector `z_e -> z_q`
+3. A `Decoder` class which defines the map `z_q -> x_hat` and reconstructs the original image
+
+The Encoder / Decoder classes are convolutional and inverse convolutional stacks, which include Residual blocks in their architecture [see ResNet paper](https://arxiv.org/abs/1512.03385). The residual models are defined by the `ResidualLayer` and `ResidualStack` classes.
+
---
+++ models/vqvae.py
@@ -0,0 +1,54 @@
+
+import torch
+import torch.nn as nn
+import numpy as np
+from models.encoder import Encoder
+from models.quantizer import VectorQuantizer
+from models.decoder import Decoder
+import clip
+
+class VQVAE(nn.Module):
+    def __init__(self, h_dim, res_h_dim, n_res_layers,
+                 n_embeddings, embedding_dim, beta, save_img_embedding_map=False):
+        super(VQVAE, self).__init__()
+        # encode image into continuous latent space
+        self.load_clip()
+        # self.encoder = Encoder(3, h_dim, n_res_layers, res_h_dim)
+        self.encoder = self.clipmodel.encode_image
+        self.TextEncoder = self.clipmodel.encode_text
+        self.pre_quantization_conv = nn.Conv2d(
+            h_dim, embedding_dim, kernel_size=1, stride=1)
+        # pass continuous latent vector through discretization bottleneck
+        self.vector_quantization = VectorQuantizer(
+            n_embeddings, embedding_dim, beta)
+        # decode the discrete latent representation
+        self.decoder = Decoder(embedding_dim, h_dim, n_res_layers, res_h_dim)
+
+        if save_img_embedding_map:
+            self.img_to_embedding_map = {i: [] for i in range(n_embeddings)}
+        else:
+            self.img_to_embedding_map = None
+
+    def load_clip(self):
+        self.clipmodel, self.clip_preprocess = clip.load("ViT-B/32")
+        self.clipmodel.cuda().eval()
+        self.input_resolution = self.clipmodel.visual.input_resolution
+        self.context_length = self.clipmodel.context_length
+        self.vocab_size = self.clipmodel.vocab_size
+
+    def forward(self, x, verbose=False):
+
+        z_e = self.encoder(x)
+
+        z_e = self.pre_quantization_conv(z_e)
+        embedding_loss, z_q, perplexity, _, _ = self.vector_quantization(
+            z_e)
+        x_hat = self.decoder(z_q)
+
+        if verbose:
+            print('original data shape:', x.shape)
+            print('encoded data shape:', z_e.shape)
+            print('recon data shape:', x_hat.shape)
+            assert False
+
+        return embedding_loss, x_hat, perplexity
